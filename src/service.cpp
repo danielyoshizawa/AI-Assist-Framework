b4b64eb436b7305b06568a28eaa25892cac17fd5
danielyoshizawa <yoshidanielcwb@gmail.com>
2024-08-18 11:49:15 -0300

    Adding health check to the service

--- src/service.cpp
+++ src/service.cpp
@@ -2,5 +2,7 @@
 
 void Service::Start(int && port) {
     std::cout << "Microservice is running on port " << port << std::endl;
+    server.Get("/api/health", [this](const httplib::Request &, httplib::Response &res)
+                   { res.set_content("OK\n", "text/plain"); });
     server.listen("localhost", port);
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -8,3 +8,15 @@ TEST_CASE("Test 1", "[test]") {
 
     REQUIRE(res->body.compare("Hello from the Service 2!\n") == 0);
 }
+
+TEST_CASE("Health Check", "[test]") {
+    httplib::Client client{"localhost:3000"}; // Service 1
+    auto res = client.Get("/api/health");
+
+    REQUIRE(res->body.compare("OK\n") == 0);
+
+    httplib::Client client2{"localhost:3001"}; // Service 2
+    res = client2.Get("/api/health");
+
+    REQUIRE(res->body.compare("OK\n") == 0);
+}
